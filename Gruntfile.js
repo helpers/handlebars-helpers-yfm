1e78def2244ad864b0342478d0bfd3fe51056fc2
Jon Schlinkert <dev@sellside.com>
2013-08-28 08:49:20 -0400

    add non-assemble examples and tests (borrowed from the assemble-front-matter project)

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,6 +9,10 @@
 
 module.exports = function(grunt) {
 
+  // Report elapsed execution time of grunt tasks.
+  require('time-grunt')(grunt);
+
+
   // Project configuration.
   grunt.initConfig({
 
@@ -31,18 +35,39 @@ module.exports = function(grunt) {
       }
     },
 
+    // Run mocha tests.
+    mochaTest: {
+      files: ['test/**/*.js']
+    },
+    mochaTestConfig: {
+      options: {
+        reporter: 'nyan'
+      }
+    },
+
     // Before generating any new files,
     // remove any previously-created files.
     clean: {
       example: ['docs/*.html']
+    },
+
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.all %>',
+        tasks: ['jshint:lint']
+      }
     }
   });
 
   // Load npm plugins to provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-mocha-test');
   grunt.loadNpmTasks('assemble');
 
+  // Tests to be run.
+  grunt.registerTask('test', ['mochaTest']);
+
   // Default to tasks to run with the "grunt" command.
-  grunt.registerTask('default', ['clean', 'jshint', 'assemble']);
+  grunt.registerTask('default', ['clean', 'jshint', 'assemble', 'test']);
 };
